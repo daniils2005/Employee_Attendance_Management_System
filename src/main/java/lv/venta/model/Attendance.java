ee94d0f7fe75981bd1644d54879916c7ce8a1de0
Daniils <daniils.strals@gmail.com>
2026-06-10 15:39:16 +0300

    Fixes in pom.xml and started working on Attendance class

--- src/main/java/lv/venta/model/Attendance.java
+++ src/main/java/lv/venta/model/Attendance.java
@@ -1,5 +1,40 @@
 package lv.venta.model;
 
-public class Attendance {
+import java.time.LocalDate;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "attendance_table")
+public class Attendance {
+	
+	@Setter(value = AccessLevel.NONE)
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "aid")
+	private long aid;
+
+	@NotNull
+	@Column(name = "work_date")
+	private LocalDate workDate;
+	
+	@Min(0)
+	@Column(name = "hours_worked")
+	private int hoursWorked;
 }
