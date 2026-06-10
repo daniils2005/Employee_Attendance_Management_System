261983a3f171419f4a4c82cdfd282ea3f6f39c25
Daniils <daniils.strals@gmail.com>
2026-06-10 14:50:40 +0300

    Initial commit

---
+++ src/main/java/lv/venta/EmployeeAttendanceManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package lv.venta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeAttendanceManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeAttendanceManagementSystemApplication.class, args);
+	}
+
+}
