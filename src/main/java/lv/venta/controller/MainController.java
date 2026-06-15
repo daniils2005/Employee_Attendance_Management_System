59cf2edf066758e0f03a3504cfb519064939c952
Daniils <daniils.strals@gmail.com>
2026-06-16 02:59:57 +0300

    Created admin-home-page.html and modified getHomePage controller method so that different roles have their own home page; improved home-page.html

--- src/main/java/lv/venta/controller/MainController.java
+++ src/main/java/lv/venta/controller/MainController.java
@@ -1,6 +1,11 @@
 package lv.venta.controller;
 
+import java.security.Principal;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,8 +14,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MainController {
     
     @GetMapping("/home")
-    public String getHomePage() {
-        return "home-page";
+    public String getHomePage(Model model, Principal principal) {
+    	model.addAttribute("username", principal.getName());
+    	Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+    	if(auth.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ADMIN"))) {
+    		return "admin-home-page";
+    	}
+    	return "home-page";
     }
     
 }
